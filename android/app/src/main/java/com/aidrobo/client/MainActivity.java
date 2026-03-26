2cf9b54c7a52d006131a681c83c09fabca841f2f
chendongfang <chendongfang@aidlux.com>
2026-03-26 09:32:34 +0800

    modified:   android/app/src/main/AndroidManifest.xml 	modified:   android/app/src/main/java/com/aidrobo/client/MainActivity.java 	modified:   index.html 	modified:   src/assets/global.less

--- android/app/src/main/java/com/aidrobo/client/MainActivity.java
+++ android/app/src/main/java/com/aidrobo/client/MainActivity.java
@@ -1,5 +1,59 @@
 package com.aidrobo.client;
 
+import android.os.Bundle;
+import android.os.Build;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (getBridge() != null && getBridge().getWebView() != null) {
+			getBridge().getWebView().getSettings().setTextZoom(100);
+			getBridge().getWebView().getSettings().setUseWideViewPort(true);
+			getBridge().getWebView().getSettings().setLoadWithOverviewMode(true);
+		}
+		hideSystemBars();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		hideSystemBars();
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		if (hasFocus) {
+			hideSystemBars();
+		}
+	}
+
+	private void hideSystemBars() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+			WindowInsetsController controller = getWindow().getInsetsController();
+			if (controller != null) {
+				controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+				controller.setSystemBarsBehavior(
+					WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+				);
+			}
+		} else {
+			View decorView = getWindow().getDecorView();
+			decorView.setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_FULLSCREEN
+					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+			);
+		}
+	}
+}
